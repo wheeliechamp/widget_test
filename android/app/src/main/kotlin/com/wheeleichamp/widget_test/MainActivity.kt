9ea1acb5611e1ad9bf2f35cb2d8d9ea270a557ca
wheeliechamp <wheeliechamp2005@yahoo.co.jp>
2023-04-21 00:37:34 +0900

    first

---
+++ android/app/src/main/kotlin/com/wheeleichamp/widget_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.wheeleichamp.widget_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/new_page1.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class _NewPage1 extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Page1"),
+      ),
+      body: Center(
+        child: Container(
+          child: Column(
+              children: [
+                Flexible(
+                  flex: 1,
+                  child: Container(
+                    color: Colors.red,
+                  ),
+                ),
+                Flexible(
+                  flex: 1,
+                  child: Container(
+                    color: Colors.blue,
+                  ),
+                ),
+
+              ]
+          ),
+        ),
+      ),
+    );
+  }
